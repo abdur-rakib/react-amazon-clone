07ca8358ad04f52d87db1e71a70e373fa38e7d35
abdur_rakib <abdurrakib961@gmail.com>
2020-07-18 00:18:55 +0600

    Updates

---
+++ precache-manifest.b7beffb4861952154bb7052a4c8a8f06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f0f8006ff2e7759706d5d61ef4e1a6a",
+    "url": "/react-amazon-clone/index.html"
+  },
+  {
+    "revision": "c1d8a145c4a71d4ee8c0",
+    "url": "/react-amazon-clone/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "453491983518d7332d7c",
+    "url": "/react-amazon-clone/static/css/main.4d5dea87.chunk.css"
+  },
+  {
+    "revision": "c1d8a145c4a71d4ee8c0",
+    "url": "/react-amazon-clone/static/js/2.1c4abcb5.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/react-amazon-clone/static/js/2.1c4abcb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "453491983518d7332d7c",
+    "url": "/react-amazon-clone/static/js/main.07492e09.chunk.js"
+  },
+  {
+    "revision": "04c0ebc6ad64b9b72b43",
+    "url": "/react-amazon-clone/static/js/runtime-main.8287139a.js"
+  }
+]);
